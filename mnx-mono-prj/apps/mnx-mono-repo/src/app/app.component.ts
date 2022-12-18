5a0ec37d10c6e18b73500f91228cd86334265717
Venkat <vbonam@kent.edu>
2022-12-17 21:33:39 -0500

    Add Prj Scaffolding

---
+++ mnx-mono-prj/apps/mnx-mono-repo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mnx-mono-prj-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'mnx-mono-repo';
+}
